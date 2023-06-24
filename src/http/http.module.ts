39a0c6b432d2ea25015184ed68a7f29b9ea188f4
Vanderson <vandimavellar1997@gmail.com>
2023-06-24 14:15:47 -0300

    add @UseGuards

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TestController } from './test.controller';
 
-@Module({ imports: [ConfigModule.forRoot()] })
+@Module({ imports: [ConfigModule.forRoot()], controllers: [TestController] })
 export class HttpModule {}
